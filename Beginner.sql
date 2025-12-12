e1fc89b193b6b27201eb7062bf29a50dcc9e1606
Sagar HK <sagarcoursera25@gmail.com>
2025-12-12 20:26:53 +0530

    Add DDL and DML commands for persons and customers

---
+++ Beginner.sql
@@ -0,0 +1,77 @@
+-- This file contains code for DDL and DML
+
+-- DDL Commands
+/* Create a new table called persons
+with columns: id, person_name , birth_date,phone */
+
+create table persons(
+	id int Not null,
+	person_name varchar(50) Not Null,
+	birth_date date,
+	phone varchar(15) not null
+	constraint pk_persons primary key (id)
+)
+
+select * from persons
+
+-- Add a new column called email to person table
+
+alter table persons
+add email varchar(50) not null
+
+-- Remove column phone from persons table 
+
+alter table persons
+drop column phone
+
+--Delete the table persons from the database
+
+drop table persons
+
+-- DML Commands
+
+-- Manually adding values to columns of data
+insert into sales.customers (customerid, firstname, country,score) -- Order should be same with values
+values (6,'Sagar','India', null),
+(7,'Kushal',Null,100)
+
+select * from sales.Customers
+
+-- using query to insert data from one table to another
+-- first lets create a table persons for which we will insert the data from customers
+create table persons(
+	id int Not null,
+	person_name varchar(50) Not Null,
+	birth_date date,
+	phone varchar(15) not null
+	constraint pk_persons primary key (id)
+)
+-- insert the data from customers table to persons table
+Insert into persons(id,person_name,birth_date,phone)
+select 
+customerid, firstname,null,'unknown'
+from sales.customers
+
+select * from persons
+
+--inorder to change the contents of already existing rows
+
+-- update the sagars score to 100
+select * from sales.Customers
+
+update sales.Customers
+set score = 100
+where firstname = 'Sagar'
+
+-- update kushal score and country
+update sales.Customers
+set score = 200,
+	country = 'India'
+where firstname = 'kushal'
+
+-- update all customers with null score to 0
+update sales.Customers
+set score = 0
+where score is null
+
+
